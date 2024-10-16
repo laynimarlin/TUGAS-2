dcfe9cce4ac60899a4b986c3eb99ea5540d05e6e
laynimarlin <laynimarlin97@gmail.com>
2024-10-16 03:18:51 -0700

    penjumlahan.py

---
+++ penjumlahan.py
@@ -0,0 +1,9 @@
+# mengambil input dari pengguna
+bilangan1 = float(input("masukkan bilangan pertama: "))
+bilangan2 = float(input("masukkan bilangan kedua: "))
+
+# menjumlahkan kedua bilangan 
+hasil = bilangan1 + bilangan2
+
+# menampilkan hasil
+print("hasil penjumlahan:", hasil)

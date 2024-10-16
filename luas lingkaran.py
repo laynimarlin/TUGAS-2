53e5ead5530b7035c0c73039cc841328d37835cc
laynimarlin <laynimarlin97@gmail.com>
2024-10-16 03:01:31 -0700

    luas lingkaran.py

---
+++ luas lingkaran.py
@@ -0,0 +1,8 @@
+# mengambil input
+jari_jari = float(input("masukan jari-jari lingkaran: "))
+
+# menghitung luas lingkaran 
+luas = (jari_jari ** 2)
+
+# menampilkan hasil
+print("luas lingkaran adalah:", luas)
